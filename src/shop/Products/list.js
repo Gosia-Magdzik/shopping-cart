074f3f9fc4374d5e99ed8dfaf89c8b3ce6f9d779
Gosia Magdzik <melassa26@gmail.com>
2023-06-07 12:36:17 +0200

    Products added

---
+++ src/shop/Products/list.js
@@ -0,0 +1,113 @@
+import Racket1 from "./images/Racket1.jpg"
+import racket2 from "./images/racket2.jpg"
+import racket3 from "./images/racket3.jpg"
+import ball4 from "./images/ball4.jpg"
+import ball12 from "./images/ball12.jpg"
+import cart from "./images/cart.jpg"
+import grip from "./images/grip.jpg"
+import bag from "./images/bag.jpg"
+import ring from "./images/ring.jpg"
+import string from "./images/string.jpg"
+import Tshirt from "./images/Tshirt.jpg"
+import skirt from "./images/shirt.jpg"
+
+
+export const list = [
+    {
+      id: 1,
+      title: "Tour Racket (315g)",
+      Collection: "Wilson Shift",
+      price: 269.95,
+      img: Racket1,
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "Tour racket Graphene 360+",
+      Collection: "HEAD - Radical",
+      price: 164,
+      img: racket2,
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Junior racket",
+      Collection: "Babolat - Pure Drive",
+      price: 74.95,
+      img: racket3,
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "4 Ball Tube",
+      Collection: "Play and Stay ball",
+      price: 8.5,
+      img: ball4,
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "Balls Unlimited | 12 Pack",
+      Collection: "Play and Stay | Stage 1 | Training balls",
+      price: 20.95,
+      img: ball12,
+      amount: 1,
+    },
+    {
+      id: 6,
+      title: "Ball cart",
+      Collection: "Teaching 150 Ball Cart With Wheels | Red",
+      price: 109.95,
+      img: cart,
+      amount: 1,
+    },
+    {
+      id: 7,
+      title: "Pre-School Activities Pack ",
+      Collection: "Wilson | Featherthin Grip | Black",
+      price: 6,
+      img: grip,
+      amount: 1,
+    },
+    {
+      id: 8,
+      title: "Tournament Bag",
+      Collection: "HEAD - Tour Team | White, Grey",
+      price: 64.95,
+      img: bag,
+      amount: 1,
+    },
+    {
+      id: 9,
+      title: "Key Ring",
+      Collection: "pink",
+      price: 2.50,
+      img: ring,
+      amount: 1,
+    },
+    {
+      id: 10,
+      title: "String Reel",
+      Collection: "Tennis-Point Classic Tour | 200m | White",
+      price: 20.95,
+      img: string,
+      amount: 1,
+    },
+    {
+      id: 11,
+      title: "Skirt Women",
+      Collection: "Wilson | 100% Nylon | Black",
+      price: 44.95,
+      img: skirt,
+      amount: 1,
+    },
+    {
+      id: 12,
+      title: "T-Shirt Women",
+      Collection: "Wilson | 100% cotton | Dark Blue",
+      price: 31.95,
+      img: Tshirt,
+      amount: 1,
+    },
+  ];
+  
