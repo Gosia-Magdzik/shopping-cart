5f3f88d306cac37fec4b168a94fe894201f34b25
Gosia Magdzik <melassa26@gmail.com>
2023-06-07 16:00:38 +0200

    info in footer added

---
+++ src/footer/index.js
@@ -0,0 +1,11 @@
+import { Wrapper } from "./styled";
+
+
+export const Footer = () => {
+    return (
+        <Wrapper>
+            All products with their prices were taken from:&nbsp; 
+            <a href="https://global.tennis-point.com/" target="_blank">tennis Point</a>&nbsp; 7.06.2023 r.
+        </Wrapper>
+    );
+}
---
+++ src/footer/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    text-align: right;
+    padding: 20px;
+    color: white;
+    font-style: italic;
+    font-size: 20px;
+`;
