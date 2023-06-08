2a4ef4d7007fc9731700772e0616ff7fa1b64065
Gosia Magdzik <melassa26@gmail.com>
2023-06-08 19:43:24 +0200

    cart component added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Nav } from "./Nav";
 import { useState, useEffect } from "react";
 import { Shop } from "./shop";
+import { Cart } from "./cart"
 import { Footer } from "./footer";
 
 export default () => {
+
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    console.log(item);
+  }
 
   const [transparent, setTransparent] = useState(true);
 
@@ -24,8 +32,9 @@ export default () => {
     return (
       <>
         <GlobalStyle />
-          <Nav transparent={transparent}/>
-          <Shop />
+          <Nav transparent={transparent} size={cart.length}/>
+          <Shop handleClick={handleClick} />
+          <Cart />
           <Footer />
       </>
     );
