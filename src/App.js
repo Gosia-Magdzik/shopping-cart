a18057d5c4215a2211e6d192ec6a4fecb76bc3c1
Gosia Magdzik <melassa26@gmail.com>
2023-06-10 14:17:11 +0200

    Information about cart added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { useState, useEffect } from "react";
 import { Shop } from "./shop";
 import { Cart } from "./cart"
 import { Footer } from "./footer";
+import { WarningStyled, WarningWrapper } from "./styled";
 
 export default () => {
 
   const [show, setShow] = useState(true);
   const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
 
   const handleClick = (item) => {
-    console.log(item);
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) {
+      isPresent = true;
+      }
+    })
+      if (isPresent) {
+        setWarning(true);
+          setTimeout (() => {
+            setWarning(false);
+          }, 2000);
+        return;
+      }
+        setCart([...cart, item]);
   }
 
   const [transparent, setTransparent] = useState(true);
@@ -32,9 +47,13 @@ export default () => {
     return (
       <>
         <GlobalStyle />
-          <Nav transparent={transparent} size={cart.length}/>
-          <Shop handleClick={handleClick} />
-          <Cart />
+          <Nav transparent={transparent} size={cart.length} setShow={ setShow }/>
+          {
+            warning && <WarningWrapper><WarningStyled>☝️ Item is already in your cart</WarningStyled></WarningWrapper>
+          } 
+          {
+            show ? <Shop handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} />      
+          }
           <Footer />
       </>
     );
---
+++ src/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const WarningWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+export const WarningStyled = styled.p`
+    padding: 20px;
+    color: #c33b3b;
+    background-color: rgba(255, 255, 255, 0.5);
+    font-style: italic;
+    font-size: 30px;
+    font-weight: bold;
+    border-radius: 10px;
+    margin-right: 40px;
+    box-shadow: 15px 15px 14px -12px black;
+
+`;
