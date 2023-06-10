50c66b1ce6da05083e5cb0824126b3ca03c7830d
Gosia Magdzik <melassa26@gmail.com>
2023-06-10 14:17:29 +0200

    cart component added

--- src/cart/index.js
+++ src/cart/index.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import { MainWrapper, CartBox, ButtonWrapper, Button, Img } from "./styled";
 
+export const Cart = ({ cart, setCart }) => {
+  
+  const [price, setPrice] = useState(0);
 
-export const Cart = () => {
     return (
-      <></>
+      <MainWrapper>
+        {
+          cart?.map((item) => {      
+            return ( 
+              <div key={item.id}>
+              <CartBox key = { item.id }>
+                <Img src = { item.img } />
+                <p> { item.title } </p>
+              </CartBox>
+              <ButtonWrapper>
+                  <Button> + </Button>
+                  <Button> - </Button>
+              </ButtonWrapper>
+                <div>
+                    <span> { item.price } </span>
+                    <Button></Button>
+                </div>
+              </div>
+            );
+          })}
+        <div>
+            <span>Total price: </span>
+            <span> { price } euro </span>
+        </div>
+      </MainWrapper>
     );
 }
---
+++ src/cart/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.article`
+
+`;
+
+export const CartBox = styled.div`
+
+`;
+
+
+export const ButtonWrapper = styled.div`
+
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const Img = styled.img`
+
+`;
+
