6fbfdc395552147c75ecf5d1a320c2876e05888d
Gosia Magdzik <melassa26@gmail.com>
2023-06-10 14:33:14 +0200

    fixed position

--- src/styled.js
+++ src/styled.js
@@ -8,12 +8,12 @@ export const WarningWrapper = styled.div`
 export const WarningStyled = styled.p`
     padding: 20px;
     color: #c33b3b;
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(255, 255, 255, 0.9);
     font-style: italic;
     font-size: 30px;
     font-weight: bold;
     border-radius: 10px;
     margin-right: 40px;
     box-shadow: 15px 15px 14px -12px black;
-
+    position: fixed;
 `;
