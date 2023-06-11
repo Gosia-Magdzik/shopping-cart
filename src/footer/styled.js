eae20b4643ca157600a479fbb5c3066ef3b4ebcc
Gosia Magdzik <melassa26@gmail.com>
2023-06-11 13:36:18 +0200

    @media

--- src/footer/styled.js
+++ src/footer/styled.js
@@ -6,4 +6,22 @@ export const Wrapper = styled.div`
     color: white;
     font-style: italic;
     font-size: 20px;
+
+    @media (max-width: 1300px) {
+        padding: 15px;
+        margin: 2px;
+        font-size: 15px;
+    };
+
+    @media (max-width: 992px) {
+        padding: 10px;
+        margin: 1px;
+        font-size: 10px;
+    };
+
+    @media (max-width: 768px) {
+        padding: 5px;
+        margin: 1px;
+        font-size: 10px;
+    };
 `;
